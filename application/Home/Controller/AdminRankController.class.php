bb041fa08495a3f300bcb6156aecb85389427166
Bin Shen <bin.shen@openx.com>
2015-05-09 00:06:58 +0800

    added staff rank pages

--- application/Home/Controller/AdminRankController.class.php
+++ application/Home/Controller/AdminRankController.class.php
@@ -4,19 +4,79 @@ use Common\Controller\AdminbaseController;
 
 class AdminRankController extends AdminbaseController {
     
-    protected $rank_model;
+    protected $Dao;
     
     function _initialize() {
         
         parent::_initialize();
-        $this->rank_model = D("Home/Rank");
+        $this->Dao = D("Home/Rank");
     }
     
     function index(){
         
-        $rank = $this->rank_model->select();
-        dump($rank);
+        $rankList = $this->Dao->select();
+        $this->assign('rankList', $rankList);
         
         $this->display();
+    }
+    
+    function add() {
+        
+        $this->display('AdminRank:edit');
+    }
+    
+    function edit() {
+        
+        $id = I('get.id', 0, 'intval');
+        $rank = $this->Dao->getById($id);
+        $this->assign($rank);
+        
+        $this->display();
+    }
+    
+    function edit_post() {
+        
+        if(empty($_POST['id'])) {
+            if ($this->Dao->create()) {
+                if ($this->Dao->add()!==false) {
+                    $this->success("添加成功！",U("AdminRank/index"));
+                } else {
+                    $this->error("添加失败！");
+                }
+            } else {
+                $this->error($this->Dao->getError());
+            }
+        } else {
+            if ($this->Dao->create()) {
+                if ($this->Dao->save()!==false) {
+                    $this->success("修改成功！",U("AdminRank/index"));
+                } else {
+                    $this->error("修改失败！");
+                }
+            } else {
+                $this->error($this->Dao->getError());
+            }
+        }
+    }
+    
+    function delete() {
+        
+        if(isset($_POST['ids'])){
+            $ids = implode(",", $_POST['ids']);
+            if ($this->Dao->where("id in ($ids)")->delete()) {
+                $this->success("删除成功！");
+            } else {
+                $this->error("删除失败！");
+            }
+        }else{
+            if(isset($_GET['id'])){
+                $id = intval(I("get.id"));
+                if ($this->Dao->delete($id)) {
+                    $this->success("删除成功！");
+                } else {
+                    $this->error("删除失败！");
+                }
+            }
+        }
     }
 }
