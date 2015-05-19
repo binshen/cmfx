e63c918360a4cf09e895081e6374fedd92cd1256
Bin Shen <bin.shen@openx.com>
2015-05-19 17:28:09 +0800

    ttt

---
+++ application/Home/Controller/AdminPerfController.class.php
@@ -0,0 +1,58 @@
+<?php
+namespace Home\Controller;
+use Common\Controller\AdminbaseController;
+
+class AdminPerfController extends AdminbaseController {
+    
+    protected $Dao;
+    protected $PerfBrokerDao;
+    protected $ProjectDao;
+    
+    function _initialize() {
+    
+        parent::_initialize();
+        $this->Dao = D("Home/Perf");
+        $this->PerfBrokerDao = D("Home/PerfBroker");
+        $this->ProjectDao = D("Home/Project");
+    }
+    
+    function index(){
+    
+        $map = array();
+        if(IS_POST) {
+            $project = I('post.project');
+            if(!empty($project)) {
+                $map['pid'] = $project;
+            }
+            
+            $date = I('post.date');
+            if(!empty($date)) {
+                $map['date'] = $date;
+            }
+        }
+        
+        $perfList = $this->Dao
+            ->join("sd_project ON sd_project.id = sd_perf.pid")
+            ->field("sd_perf.*, sd_project.name AS pname")
+            ->where($map)
+            ->select();
+
+//         $pidList = array();
+//         foreach ($perfList as $p) {
+//             $pidList[] = $p['id'];
+//         }
+//         $map['sd_perf_broker.pid'] = array("IN", implode(",", $pidList));
+//         $pbList = $this->PerfBrokerDao
+//             ->join("sd_broker ON sd_broker.id = sd_perf_broker.bid")
+//             ->field("sd_perf_broker.*, sd_broker.name AS bname")
+//             ->where($map)
+//             ->select();
+        
+        $projectList = $this->ProjectDao->select();
+        $this->assign('projectList', $projectList);
+        
+        $this->assign('perfList', $perfList);
+    
+        $this->display();
+    }
+}
---
+++ application/Home/Model/PerfBrokerModel.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Model;
+use Common\Model\CommonModel;
+
+class PerfBrokerModel extends CommonModel{
+    
+    protected $tablePrefix = 'sd_';
+}
