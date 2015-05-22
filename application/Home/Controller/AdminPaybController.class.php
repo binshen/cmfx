ddecdb78a1323482e4d338b54d8f784d93c8ec4a
yaobin <bluelazysb@hotmail.com>
2015-05-22 17:24:40 +0800

    sss

--- application/Home/Controller/AdminPaybController.class.php
+++ application/Home/Controller/AdminPaybController.class.php
@@ -9,37 +9,66 @@ class AdminPaybController extends AdminbaseController {
     function _initialize() {
         
         parent::_initialize();
-        $this->Dao = D("Home/Paya");
-        $this->Daob = D("Home/Payb");
+        $this->Daoperf = D("Home/Perf");
         $this->Daol = D("Home/PayLog");
+        $this->Daop = D("Home/Project");
     }
     
     function index(){
-    	$paybList = $this->Dao
+/*    	$paybList = $this->Dao
     		->join("sd_payb ON sd_paya.id = sd_payb.pay_id","left")
     		->join("sd_project ON sd_paya.pid = sd_project.id","left")
     		->field("sd_paya.*, sum(pay) pay,sum(payd) payd,name")
     		->group("pay_id")
     		->select();
     	$this->assign('paybList', $paybList);
+        $this->display('AdminPayb:index');*/
+    	
+    	$count = $this->Daoperf
+	    	->join("sd_project ON sd_perf.pid = sd_project.id","left")
+        	->field("1")
+	        ->group("pid,date")
+	        ->select();
+        
+    	$page = $this->page(count($count), 20);
+    	
+        $paybList = $this->Daoperf
+        	->join("sd_project ON sd_perf.pid = sd_project.id","left")
+        	->field("sd_perf.*, sum(bkg) pay,sum(payd) payd,name,DATE_FORMAT(date,'%Y-%m') date")
+        	->order("DATE_FORMAT(date,'%Y-%m') desc")
+	        ->group("pid,DATE_FORMAT(date,'%Y-%m')")
+	        ->limit($page->firstRow . ',' . $page->listRows)
+	        ->select();
+        
+        $this->assign("page", $page->show('Admin'));
+
+        $this->assign('paybList', $paybList);
         $this->display('AdminPayb:index');
+        
+        
+        
     }
     
     function edit(){
-    	$id = I('get.id', 0, 'intval');
+    	$pid = I('get.pid', 0, 'intval');
+    	$date = I('get.date');
     	
-    	$paya = $this->Dao
-	    	->join("sd_project ON sd_paya.pid = sd_project.id","left")
-	    	->where("sd_paya.id=".$id)
-	    	->field("sd_paya.*,name")
+    	$paya = $this->Daop
+	    	->where("id=".$pid)
+	    	->field("name")
 	    	->find();
+    	$paya['date'] = $date;
     	
-    	$payb = $this->Daob
-    		->join("sd_broker ON sd_payb.bid = sd_broker.id","left")
-    		->join("sd_rank ON sd_broker.rank_id = sd_rank.id","left")
-    		->where('pay_id='.$id)
-    		->field("sd_payb.*,sd_broker.name bname,tel,sd_rank.name rname")
+    	
+    	$payb = $this->Daoperf
+        	->join("sd_broker ON sd_perf.bid = sd_broker.id","left")
+        	->join("sd_rank ON sd_broker.rank_id = sd_rank.id","left")
+    		->where('pid='.$pid)
+    		->where("DATE_FORMAT(sd_perf.date,'%Y-%m')='".$date."'")
+    		->field("sd_perf.*,sd_broker.name bname,tel,sd_rank.name rname")
     		->select();
+    	
+    	
     	$this->assign('payb', $payb);
     	$this->assign($paya);
     	$this->display();
@@ -48,47 +77,26 @@ class AdminPaybController extends AdminbaseController {
     
     function edit_post() {
     	$id = $_POST['id'];
-		$bid = $_POST['bid'];
 		$pay = $_POST['pay'];
-		
-		foreach($bid as $k=>$v){
+		foreach($id as $k=>$v){
 			if($pay[$k]){
-				$map =array(
-						'pay_id'=>$id,
-						'bid'=>$v
-				
-				);
+
 				$log = array(
-						'bid'=>$v,
 						'pay_id'=>$id,
 						'pay'=>$pay[$k],
 						'cdate'=>date('Y-m-d H:i:s',time())
 				);
 				
-				$this->Daob->where($map)->setInc('payd',$pay[$k]);
+				$this->Daoperf->where('id='.$id[$k])->setInc('payd',$pay[$k]);
 				$this->Daol->add($log);
 				
 			}
 		}
 		
-		$res = $this->Daob
-		->where('pay_id='.$id)
-		->field(" sum(pay) pay,sum(payd) payd")
-		->group("pay_id")
-		->find();
-		
-		if($res['payd'] >= $res['pay']){
-			$this->Dao->where('id='.$id)->data(array('status'=>2))->save();
-		}else{
-			$this->Dao->where('id='.$id)->data(array('status'=>1))->save();
-		}
-		
-		
-		//$this->success("保存成功！",U("AdminPayb/index"));
 		$this->index();
 
     }
-    
+
     
     
 }
