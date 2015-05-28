13c24063225c0b5586c4921dcb18849617c9261d
Bin Shen <bin.shen@openx.com>
2015-05-28 17:21:53 +0800

    debugging

--- application/Common/Common/function.php
+++ application/Common/Common/function.php
@@ -1465,10 +1465,9 @@ function getSelfStoreProfit($sid, $month) {
         $totalPerf = 0;
     }
     
-    
     $loss = getSelfStoreLoss($sid,$month);
 
-    return $totalPerf - $loss;
+    return $totalPerf * 0.3 - $loss;
 }
 
 function getSelfStoreLoss($sid, $month){
