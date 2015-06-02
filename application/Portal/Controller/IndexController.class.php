14284701565357c8d4f7f52055346606fce64fa7
yaobin <bluelazysb@hotmail.com>
2015-06-02 15:12:50 +0800

    222

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -16,8 +16,8 @@ class IndexController extends HomeBaseController {
     //首页
 	public function index() {
 	    
-    	redirect('/admin');
-    	//$this->display(":index");
+    	//redirect('/admin');
+    	$this->display(":index");
     }   
 
 }
